f1b63c12622a92f5b3b30513af69e44c6940d212
bdavis83 <bdavis83@gmail.com>
2022-11-03 12:25:55 -0400

    testing new setup

---
+++ main.py
@@ -0,0 +1,20 @@
+# (5 points): As a developer, I want to make at least three commits with descriptive messages. 
+# (5 points): As a developer, I want to store my destinations, restaurants, mode of transportation, and entertainment selections in their own separate lists. 
+# (5 points): As a user, I want a destination to be randomly selected for my day trip. 
+# (5 points): As a user, I want a restaurant to be randomly selected for my day trip. 
+# (5 points): As a user, I want a mode of transportation to be randomly selected for my day trip. 
+# (5 points): As a user, I want a form of entertainment to be randomly selected for my day trip. 
+# (15 points): As a user, I want to be able to randomly re-select a destination, restaurant, mode of transportation, and/or form of entertainment if I don’t like one or more of those things. 
+# (10 points): As a user, I want to be able to confirm that my day trip is “complete” once I like all of the random selections. 
+# (10 points): As a user, I want to display my completed trip in the console. 
+# (5 points): As a developer, I want all of my functions to have a Single Responsibility. Remember, each function should do just one thing!
+# '''
+
+import random
+
+destination = ['Detroit', 'Mordor', 'Gondolin','Paris', 'Numenor', 'Boyne City', 'Bozeman', 'Auckland']
+restaurants = ['In N Out Burger', 'MAP Brewery', 'Hells Pizza', 'The Leaky Cauldron', 'Streetside Seafood', 'Bilbos Pizza', 'Dog and Pony Show', 'Als Backyard', ]
+entertainment = ['Skiing', 'Hunting Sauron', 'Drinking Local Grog', 'Sailing', 'Hiking', 'Live Music', 'Archery']
+tranportation = ['Boat', 'Train', 'Floating Piece of Driftwood', 'Snowcat', 'Horse', 'Spitting Llama', '1970s Hearse']
+
+# this is a test
